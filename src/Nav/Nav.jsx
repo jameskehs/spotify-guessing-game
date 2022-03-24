8325759ddf8d8a235c1d9cf7b4e14124c04aea7e
jameskehs <100805502+jameskehs@users.noreply.github.com>
2022-03-24 18:08:13 -0500

    Update Nav.jsx

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,6 +1,4 @@
 import "./Nav.css";
-const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=b19ba0b304904f03862cf481cc44f169&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 const Nav = ({ accessToken }) => {
   function logout() {
@@ -15,7 +13,7 @@ const Nav = ({ accessToken }) => {
           LOGOUT
         </a>
       ) : (
-        <a href={AUTH_URL}>LOGIN</a>
+        <a href="https://accounts.spotify.com/authorize?client_id=b19ba0b304904f03862cf481cc44f169&response_type=code&redirect_uri=https://guess-your-library.herokuapp.com/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state">LOGIN</a>
       )}
     </nav>
   );
