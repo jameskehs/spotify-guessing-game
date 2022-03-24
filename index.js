11661828f2d47c5ddd18e9db51069f4d4d727d06
jameskehs <jameskehsdev@gmail.com>
2022-03-24 16:40:05 -0500

    Fixed express port to config with Heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 const cors = require("cors");
 const spotifyWebApi = require("spotify-web-api-node");
